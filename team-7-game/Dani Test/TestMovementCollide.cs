41b9b06534dd84654676bd66aa9f3375512f7f8d
DanielRobi05 <daniel.robi@tuni.fi>
2026-02-18 23:14:18 +0200

    Add timer to Snorp

--- team-7-game/Dani Test/TestMovementCollide.cs
+++ team-7-game/Dani Test/TestMovementCollide.cs
@@ -7,11 +7,12 @@ public partial class TestMovementCollide : CharacterBody2D
 	[Export] public float Speed = 300.0f;
 	[Export] public float speedIncrease = 50.0f;
 	private Vector2 direction;
+	[Export] private bool canMove = false;
 
-	public override void _Ready()
+	public override async void _Ready()
 	{
-		Random i = new Random();
-		int startDirection = i.Next(0, 4);
+		// Randomize start movement direction
+		int startDirection = GD.RandRange(0, 3);
 
 		switch (startDirection)
 		{
@@ -20,10 +21,20 @@ public partial class TestMovementCollide : CharacterBody2D
 			case 2: direction = new Vector2(-1, 1).Normalized(); break;
 			case 3: direction = new Vector2(1, 1).Normalized(); break;
 		}
+
+		// Create timer so Glorp wont move immediately
+		await ToSignal(
+			GetTree().CreateTimer(3.0f),
+			SceneTreeTimer.SignalName.Timeout
+		);
+
+		canMove = true;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
+		if (!canMove) return;
+
 		// Create Velocity and local _velocity
 		Vector2 _velocity = direction * Speed;
 		Velocity = _velocity;
@@ -32,6 +43,7 @@ public partial class TestMovementCollide : CharacterBody2D
 		var collision = MoveAndCollide(Velocity * (float)delta);
 
 		// if collides Bounces in the relative direction
+		// also increases Speed everytime by speedIncrease amount
 		if (collision != null)
 		{
 			direction = direction.Bounce(collision.GetNormal());
