c3063e6ade05d0261ea74e75fb960e2562fb00e8
Kirmo Lassheikki <kirmo.lassheikki@tuni.fi>
2026-03-09 10:23:26 +0200

    start from menu scene. Quit button works

--- team-7-game/Scripts/Menu.cs
+++ team-7-game/Scripts/Menu.cs
@@ -4,16 +4,28 @@ using System.Diagnostics.CodeAnalysis;
 
 public partial class Menu : Control
 {
-	public override void _Ready()
+    public override void _Ready()
     {
-		// When button nappi pressed it class the method OnPlayPressed and that method opens new scene
+        // When button nappi pressed it class the method OnPlayPressed and that method opens new scene
         Button nappi = GetNode<Button>("Nappi");
         nappi.Pressed += OnPlayPressed;
+
+
+
+        // When button nappi pressed it class the method OnPlayPressed and that method opens new scene
+        Button quit = GetNode<Button>("Quit");
+        quit.Pressed += OnQuitPressed;
     }
 
     private void OnPlayPressed()
     {
-		// When this method is called it opens the games MainScene where the main game is running
+        // When this method is called it opens the games MainScene where the main game is running
         GetTree().ChangeSceneToFile("res://Scenes/MainScene.tscn");
+    }
+
+    private void OnQuitPressed()
+    {
+        // quits
+        GetTree().Quit();
     }
 }
