70af4479adee4ab965231d7b2aa95a5b8cc3ee0a
Eelis <e.puolamaki@gmail.com>
2026-03-29 18:55:59 +0300

    Update

--- team-7-game/Scripts/PauseUI.cs
+++ team-7-game/Scripts/PauseUI.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class PauseUI : Control
 {
+	[Export] private Control _pauseMenu = null;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +12,15 @@ public partial class PauseUI : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+	public void OpenPause()
+	{
+		_pauseMenu.Show();
+		//GameManager.Instance.SceneTree.Paused = true;
+	}
+	public void ClosePause()
+	{
+		//GameManager.Instance.SceneTree.Paused = false;
+		_pauseMenu.Hide();
 	}
 }
