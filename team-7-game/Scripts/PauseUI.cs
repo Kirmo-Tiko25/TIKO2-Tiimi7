8030adc304400f5f7ec595d5126d6280630a265c
Eelis <e.puolamaki@gmail.com>
2026-04-06 22:05:22 +0300

    Update

--- team-7-game/Scripts/PauseUI.cs
+++ team-7-game/Scripts/PauseUI.cs
@@ -3,6 +3,7 @@ using System;
 
 public partial class PauseUI : Control
 {
+	private AudioStreamPlayer _buttonSound;
 	[Export] private Control _pauseMenu = null;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,15 +15,20 @@ public partial class PauseUI : Control
 		TextureButton quitButton = GetNode<TextureButton>("Quit");
 		quitButton.Pressed += OnQuitPressed;
 
+		// Gets the node PauseClick
+		_buttonSound = GetNode<AudioStreamPlayer>("PauseClick");
+
 	}
 	private void OnContinuePressed()
 	{
+		_buttonSound.Play();
 		GetTree().Paused = false;
 		GD.Print("Unpaused");
 		QueueFree();
 	}
 	private void OnQuitPressed()
 	{
+		_buttonSound.Play();
 		GetTree().Paused = false;
 		GD.Print("Unpaused");
 		GameManager.ResetPoints();
