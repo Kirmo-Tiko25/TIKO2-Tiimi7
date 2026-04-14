36a2b4b7847b3b0f4f48ca5bd08e2fcbbc5c3ec5
DanielRobi05 <daniel.robi@tuni.fi>
2026-04-14 18:23:21 +0300

    Add tutorial video to start of game
    still has placeholder video on it

--- team-7-game/Scenes/TutorialPlayer.cs
+++ team-7-game/Scenes/TutorialPlayer.cs
@@ -8,6 +8,10 @@ public partial class TutorialPlayer : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// pause tree, but keep this node processing so that the buttons work
+		GetTree().Paused = true;
+		ProcessMode = ProcessModeEnum.Always;
+
 		GD.Print("TutorialPlayer ready");
 
 		ExitButton.Pressed += OnExitButtonPressed;
@@ -17,12 +21,17 @@ public partial class TutorialPlayer : Node2D
 
 	private void OnExitButtonPressed()
 	{
+		GetTree().Paused = false;
+
 		QueueFree();
 	}
 
 	private void OnDontShowAgainButtonPressed()
 	{
+		GetTree().Paused = false;
+
 		GameManager.TutorialToggled();
+		
 		QueueFree();
 	}
 }
